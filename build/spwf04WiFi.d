67cf8118b8ad87a17735b4647d0713681ef09b91
Garnet Grimm <garnetgrimm720@gmail.com>
2021-03-18 09:15:07 -0400

    init commit

---
+++ build/spwf04WiFi.d
@@ -0,0 +1,157 @@
+build/spwf04WiFi.o: Middlewares/ST/STM32_SPWF0xSy/Utils/spwf04WiFi.c \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/enum.h \
+ Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_module.h \
+ Drivers/BSP/X-NUCLEO-IDW0xx1/stm32_spwf_wifi.h Core/Inc/wifi_conf.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ Core/Inc/stm32f4xx_hal_conf.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/mpu_armv7.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_qspi.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cec.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_fmpi2c.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spdifrx.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dfsdm.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_lptim.h \
+ Drivers/BSP/STM32F4xx-Nucleo/stm32f4xx_nucleo.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_dma.h \
+ Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_const.h \
+ Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_interface.h \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/event_buffer.h \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/ring_buffer.h \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/spwf04WiFi.h \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/wifi_globals.h \
+ Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_const.h \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/event_buffer.h \
+ Middlewares/ST/STM32_SPWF0xSy/Utils/ring_buffer.h
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/enum.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_module.h:
+
+Drivers/BSP/X-NUCLEO-IDW0xx1/stm32_spwf_wifi.h:
+
+Core/Inc/wifi_conf.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+Core/Inc/stm32f4xx_hal_conf.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+Drivers/CMSIS/Include/core_cm4.h:
+
+Drivers/CMSIS/Include/cmsis_version.h:
+
+Drivers/CMSIS/Include/cmsis_compiler.h:
+
+Drivers/CMSIS/Include/cmsis_gcc.h:
+
+Drivers/CMSIS/Include/mpu_armv7.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_qspi.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cec.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_fmpi2c.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spdifrx.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dfsdm.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_lptim.h:
+
+Drivers/BSP/STM32F4xx-Nucleo/stm32f4xx_nucleo.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_dma.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_const.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_interface.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/event_buffer.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/ring_buffer.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/spwf04WiFi.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/wifi_globals.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Inc/wifi_const.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/event_buffer.h:
+
+Middlewares/ST/STM32_SPWF0xSy/Utils/ring_buffer.h:
